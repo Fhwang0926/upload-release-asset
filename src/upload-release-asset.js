aee0f18c9728b305eb27be2f27379ded2f5e32e8
Fhwang <hdh0926@naver.com>
2020-06-26 00:44:25 +0900

    test

--- src/upload-release-asset.js
+++ src/upload-release-asset.js
@@ -1,16 +1,19 @@
 const core = require('@actions/core');
-const GitHub = require('@actions/github');
+const { GitHub } = require('@actions/github');
 const fs = require('fs');
+const mime = require('mime-types')
 
 async function run() {
   try {
+    //  [0]node [1]src/main.js [2]--debug [3]url [4]name
+    const debug = process.argv.indexOf('--debug') > -1 ? true : false
     // Get authenticated GitHub client (Ocktokit): https://github.com/actions/toolkit/tree/master/packages/github#usage
     const github = new GitHub(process.env.GITHUB_TOKEN);
 
     // Get the inputs from the workflow file: https://github.com/actions/toolkit/tree/master/packages/core#inputsoutputs
-    const uploadUrl = core.getInput('upload_url', { required: true });
-    const assetName = core.getInput('asset_name', { required: true });
-    const assetContentType = core.getInput('asset_content_type', { required: true });
+    const uploadUrl = debug ? process.argv[3] : core.getInput('upload_url', { required: true });
+    const assetName = debug ? process.argv[4] : core.getInput('asset_name', { required: true });
+    // const assetContentType = debug ? core.getInput('asset_content_type', { required: true }) : ;
 
     // optional
     const assetPath = core.getInput('asset_path', { required: false });
@@ -18,10 +21,17 @@ async function run() {
 
     // Determine content-length for header to upload asset
     const contentLength = filePath => fs.statSync(filePath).size;
+    const assetContentType = target => {
+      if (target.indexOf('.')) {
+        const f = target.split('.')
+        return mime.lookup(f[f.length - 1])
+      }
+      return 'text/plain'
+    }
 
     // Setup headers for API call, see Octokit Documentation: https://octokit.github.io/rest.js/#octokit-routes-repos-upload-release-asset for more information
     const target = assetPath !== '' ? assetPath : assetName;
-    const headers = { 'content-type': assetContentType, 'content-length': contentLength(target) };
+    const headers = { 'content-type': assetContentType(target), 'content-length': contentLength(target) };
 
     // Upload a release asset
     // API Documentation: https://developer.github.com/v3/repos/releases/#upload-a-release-asset
@@ -35,20 +45,17 @@ async function run() {
     }
 
     // eslint-disable-next-line no-console
-    console.log(`request : ${JSON.stringify({
-        url: uploadUrl.replace('{?name,label}', queryString),
-        headers,
-        name: assetName,
-        file: fs.readFileSync(assetPath !== '' ? assetPath : assetName)
-      })}`
-    );
-
-    const uploadAssetResponse = await github.repos.uploadReleaseAsset({
-      url: uploadUrl.replace('{?name,label}', queryString),
+    const request = {
+      url: uploadUrl.replace('?{name,label}', queryString),
       headers,
       name: assetName,
-      file: fs.readFileSync(assetPath !== '' ? assetPath : assetName)
-    });
+      data: fs.readFileSync(target)
+    };
+    if (debug) {
+      console.log(`request : ${JSON.stringify(request)}`);
+    }
+
+    const uploadAssetResponse = await github.repos.uploadReleaseAsset(request);
 
     // Get the browser_download_url for the uploaded release asset from the response
     const {
